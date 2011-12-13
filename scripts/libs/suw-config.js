3df556d978910e2e774281d3a65be5c7be30ca15
Francesco <jabba.dev@gmail.com>
2011-12-13 23:40:30 +0100

    correct example configuration

--- scripts/libs/suw-config.js
+++ scripts/libs/suw-config.js
@@ -2,21 +2,19 @@
 	var wrlc = config.wrlcNS;
 	
 	function wrlc() {
-			this.jsList = {	
-				jsA: { name : "jsA", url : "/jdocgui/docgui/js/plugins/doc-metas/js/jquery.docgui.docMetas.js", isNotLoaded : true, isNotLocked : true,
-					preLoadActivity : function() { return true; }, postLoadActivity : function() { return true; }, depon : [ "jquery" ] },
-				jsB: { name : "ricNAssegno", url : "/jdocgui/docgui/js/plugins/ric-nassegno/js/jquery.docgui.ricNAssegno.js", isNotLoaded : true, isNotLocked : true,
-					preLoadActivity : function() { return true; }, postLoadActivity : function() { return true; }, depon : [ "baseWidget" ] }
+			this.js = {	
+				jsA: { url : "/.../xxx.js" },
+				jsB: { url : "/.../yyy.js", depon : [ "jsA" ] }
 			};
 			
-			this.cssList = {
-				cssA : { name : "cssA", url : "/jdocgui/docgui/js/plugins/doc-metas/css/docMetas.css", isNotLoaded : true, isNotLocked : true },
-				cssB : { name : "cssB", url : "/jdocgui/docgui/js/plugins/docgui-widget/css/docgui-table.css", isNotLoaded : true, isNotLocked : true },
+			this.css = {
+				cssA : { url : "/.../s.css" },
+				cssB : { url : "/.../table.css" },
 			};
 			
-			this.htmlList = {
-				htmlA : { url : "/jdocgui/docgui/views/plugins/docMetas.html" },
-				htmlB : { url : "/jdocgui/docgui/js/plugins/doc-metas/html/docMetas.html" }
+			this.html = {
+				htmlA : { url : "/..../s.html" },
+				htmlB : { url : "/..../tas.html" }
 			};
 	};
 	
